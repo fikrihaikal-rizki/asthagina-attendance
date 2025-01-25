f83cc3b9cf508d1b22ca90dd0e318379047fe2ee
fikri <fikri.hairizki@gmail.com>
2025-01-26 01:52:37 +0700

    [INITIAL] - Add shadcn-vue card, button, select, input & label

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
-import Login from '@/pages/Login.vue'
+import Container from './components/Container.vue';
+import Login from './pages/Login.vue';
 </script>
 
 <template>
-  <Login></Login>
+  <div class="h-screen content-center gap-6 rounded-lg p-8">
+      <Container>
+        <Login></Login>
+      </Container>
+  </div>
 </template>
